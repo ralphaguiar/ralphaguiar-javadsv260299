78bd61da18859e521d780a62254de8c318a16af6
Ralph Aguiar <ralphaguiar@gmail.com>
2021-02-27 10:36:10 -0300

    Projeto 27022021

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ public class Program {
 		System.out.println("teste github");
 		System.out.println("teste github");
 		System.out.println("teste github");
+		System.out.println("teste github --------------");
 
 	}
 
